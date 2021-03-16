90ec2d5c5cd17efc5d12d9570cfa6b3e762607e3
Garret <gwiats@gmail.com>
2021-03-16 15:01:36 -0400

    commit tree f2df5cb57f95a6bd41037218e437198782817f4b

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(dist){
+  if(dist <= 400) {
+    return ('This one is on me!')
+    //debugger;
+  } else if (dist <=2500) {
+    return ('I will gladly take your thirty bucks.')
+  } else {
+    return ('No can do.')
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return ('Thank you so much.')
+    case 'not as generous':
+      return ('Thank you.')
+    case 'thanks for everything':
+      return ('Bye.')
+  }
+
 }
+
+scuberGreetingForFeet(37);
